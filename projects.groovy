622369fba99f6f83545ebe7bb0d7191f4dd1a584
Piotr Szubiakowski <pszubiak@eso.org>
2019-06-12 12:51:23 +0200

    adds release job

--- projects.groovy
+++ projects.groovy
@@ -1,12 +1,12 @@
 def final projects = [
         'project_a': [
-                scmurl: 'https://github.com/pszubiak/dsl-example-2',
+                github: 'pszubiak/dsl-example-2',
                 release_script: 'scripts/release.sh',
                 nightly_build_script: 'scripts/pr.sh',
         ],
 
         'project_b': [
-                scmurl: 'https://github.com/pszubiak/dsl-example-2',
+                github: 'pszubiak/dsl-example-2',
                 release_script: 'scripts/release.sh',
                 nightly_build_script: 'scripts/nightly.sh',
         ]
@@ -14,5 +14,18 @@ def final projects = [
 
 
 for (project in projects) {
+
+    // Creates or updates a folder.
     folder(project.key)
+
+    // Creates or updates a free style job.
+    job("${project.key}/${project.key}-release-job") {
+
+        // Allows a job to check out sources from an SCM provider.
+        scm {
+
+            //Adds a Git SCM source for a GitHub repository.
+            github($project.get('github')
+        }
+    }
 }
