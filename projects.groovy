b248945ee8a36694f8a868a5487b540fe7cc3b59
Piotr Szubiakowski <pszubiak@eso.org>
2019-06-12 13:23:02 +0200

    Adds shell scripts

--- projects.groovy
+++ projects.groovy
@@ -1,11 +1,11 @@
 def final projects = [
-        'project_a': [
+        'project-a': [
                 github: 'pszubiak/dsl-example-2',
                 release_script: 'scripts/release.sh',
                 nightly_build_script: 'scripts/pr.sh',
         ],
 
-        'project_b': [
+        'project-b': [
                 github: 'pszubiak/dsl-example-2',
                 release_script: 'scripts/release.sh',
                 nightly_build_script: 'scripts/nightly.sh',
@@ -25,7 +25,32 @@ for (project in projects) {
         scm {
 
             //Adds a Git SCM source for a GitHub repository.
-            github(project.github)
+            github(project.value.github)
+        }
+
+        // Adds build steps to the jobs.
+        steps {
+
+            // Runs a shell script.
+            shell(readFileFromWorkspace(project.value.release_script))
+        }
+    }
+
+    // Creates or updates a free style job.
+    job("${project.key}/${project.key}-nightly-job") {
+
+        // Allows a job to check out sources from an SCM provider.
+        scm {
+
+            //Adds a Git SCM source for a GitHub repository.
+            github(project.value.github)
+        }
+
+        // Adds build steps to the jobs.
+        steps {
+
+            // Runs a shell script.
+            shell(readFileFromWorkspace(project.value.nightly_script))
         }
     }
 }
---
+++ scripts/nightly.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "Do nightly build."
